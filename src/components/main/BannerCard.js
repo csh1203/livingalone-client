3619ef19c764be1905b60b73f31d1c422c6e04aa
hyeg0121 <w2218@e-mirim.hs.kr>
2023-12-12 22:02:27 +0900

    feat: 배너 카드 컴포넌트 생성

---
+++ src/components/main/BannerCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../../css/main/BannerCard.module.css';
+import '../../css/common/Style.css';
+import { Icon } from '@iconify/react';
+
+const BannerCard = ({ iconUrl, mainTitle, subTitle, linkUrl }) => {
+    return (
+        <div className={styles['banner-card']}>
+            <h6 className={styles['sub-title']}>{subTitle}</h6>
+            <h2 className={styles['main-title']}>{mainTitle}</h2>
+            <img className={styles['icon']} src={iconUrl} alt="Icon" />
+            <a className={styles['link']} href={linkUrl} target="_blank" rel="noopener noreferrer">
+                바로가기 <Icon className={styles['iconify']} icon="uiw:right" color="#036ce7" />
+            </a>
+        </div>
+    );
+};
+
+export default BannerCard;
