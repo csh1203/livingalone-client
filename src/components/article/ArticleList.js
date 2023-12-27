d12ebe9dc1d9219c3ea252e52e3ad845bc4b2db0
hyeg0121 <w2218@e-mirim.hs.kr>
2023-12-27 17:13:23 +0900

    feat: 글 목록 UI 구현, article 컴포넌트 추가

--- src/components/article/ArticleList.js
+++ src/components/article/ArticleList.js
@@ -2,6 +2,26 @@ import React, {useState} from "react";
 import styles from "../../css/article/ArticleList.module.css"
 import {Icon} from '@iconify/react';
 
+function Article() {
+    return (
+        <div className={styles['article-container']}>
+            <div className={styles['text-content']}>
+                <div className={styles['main-contents']}>
+                    <div className={styles['article-title']}>자취방 구하는 꿀팁!</div>
+                    <div className={styles['article-date']}>2023.10.3</div>
+                </div>
+                <div className={styles['sub-contents']}>
+                    <div className={styles['article-content']}>자취방을 구하는 가장 좋은 방법에 대해 알아볼게요. 자취는 부모님 혹은 보호자의 품을
+                        떠나 독립하는것을 의미하는데요, 저는 23살때 처음 자취를 시작했었어요. 모르는것도 힘든 것도 너무 많았었던 자취이기에 다른 분들은 이런 고통을 겪지 않았으면
+                        해서 포스팅 올려봅니다. 이렇게 자취에 대해 알아보았는데요 도움이 많이 되셨나요? 도움..
+                    </div>
+                    <div className={styles['article-likes']}>추천 16</div>
+                </div>
+            </div>
+            <div className={styles['thumbnail']}></div>
+        </div>
+    );
+}
 function ArticleList() {
 
     const [selectedCategory, setSelectedCategory] = useState(1);
@@ -13,6 +33,7 @@ function ArticleList() {
 
     return (
         <div id={styles['main']}>
+            {/*카테고리*/}
             <div className={styles['category-container']}>
                 {/*줄 맞춤을 위한 div*/}
                 <div className={`${styles['category-item']} ${styles['no-category']}`}></div>
@@ -59,11 +80,19 @@ function ArticleList() {
                 {/*줄 맞춤을 위한 div*/}
                 <div className={`${styles['category-item']} ${styles['no-category']}`}></div>
             </div>
+            {/*검색 필터*/}
             <div className={styles['filter-container']}>
                 <div className={styles['filter-item']}>
                     <div className={styles['filter-text']}>검색 필터</div>
                     <Icon icon="bi:filter" className={styles['filter-icon']}/>
                 </div>
+            </div>
+            {/*글 목록*/}
+            <div className={styles['article-list']}>
+                <Article/>
+                <Article/>
+                <Article/>
+                <Article/>
             </div>
         </div>
     );
