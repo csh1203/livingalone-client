d17cb9804dd02b7e51d134889498d790ceef4f29
hyeg0121 <w2218@e-mirim.hs.kr>
2023-12-27 15:04:04 +0900

    feat: 카테고리 페이지 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Routes
+    BrowserRouter as Router,
+    Link,
+    Route,
+    Routes
 } from 'react-router-dom';
 
 import Main from "./pages/Main";
 import LoginPage from "./pages/LoginPage";
 import CostCalculatorPage from "./pages/CostCalculatorPage";
 import JoinPage from "./pages/JoinPage";
+import ArticlePage from "./pages/ArticlePage";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path={"/"} element={<Main />} />
-        <Route path={"/login"} element={<LoginPage />} />
-        <Route path={"/join"} element={<JoinPage />} />
-        <Route path={"/costCalculator"} element={<CostCalculatorPage />} />
-    </Routes>
-  </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path={"/"} element={<Main/>}/>
+                <Route path={"/login"} element={<LoginPage/>}/>
+                <Route path={"/join"} element={<JoinPage/>}/>
+                <Route path={"/costCalculator"} element={<CostCalculatorPage/>}/>
+                <Route path={"/articles"} element={<ArticlePage/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
