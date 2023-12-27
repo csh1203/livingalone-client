539810106d4a15db4a965be04493f2ae32f6bd8f
hyeg0121 <w2218@e-mirim.hs.kr>
2023-12-27 14:12:15 +0900

    refactor: 태그 작성 스타일 변경

--- src/pages/JoinPage.js
+++ src/pages/JoinPage.js
@@ -2,12 +2,13 @@ import React from "react";
 import Nav from "../components/common/Nav";
 import Footer from "../components/common/Footer";
 import Join from "../components/auth/Join";
+
 function JoinPage() {
     return (
         <div>
-            <Nav></Nav>
-             <Join></Join>
-            <Footer></Footer>
+            <Nav/>
+            <Join/>
+            <Footer/>
         </div>
     );
 }
