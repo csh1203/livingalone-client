205a3f7863b4dbb211ffcfc3760e1d4781476f59
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-02 22:25:14 +0900

    feat: qna 글 상세 컴포넌트

---
+++ src/components/qna/QnAPost.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import '../../css/common/Style.css';
+import styles from '../../css/qna/QnAPost.module.css';
+
+function QnAPost() {
+    const { id } = useParams();
+    console.log(id);
+    const [post, setPost] = useState(null);
+    const [loading, setLoading] = useState(true); // 추가: 로딩 상태 추가
+
+    useEffect(() => {
+        fetchData(); // async 함수를 사용하므로 async 키워드를 useEffect 내부에서 직접 사용하지 않음
+    }, [id]);
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(`http://localhost:3001/questions/${id}`);
+            setPost(response.data.question);
+            setLoading(false); // 추가: 데이터 로딩이 끝났으므로 로딩 상태 변경
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    // 추가: 로딩 중일 때 로딩 상태를 표시
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    // 데이터가 있을 때만 렌더링
+    return (
+        <div className={styles['container']}>
+            <div className={styles['title-container']}>
+                <span className={styles['q']}>Q.</span>
+                <span className={styles['title']}>{post.title}</span>
+            </div>
+
+            <div className={styles['content-container']}>
+                {post.content}
+            </div>
+
+            <div className={styles['info-container']}>
+                <span className={styles['user-name']}>{post.user_id}</span>
+                <div className={styles['date-and-views']}>
+                    <span className={styles['date']}>{post.createdAt.split('T')[0].split('-').join('.')}</span>
+                    <span className={styles['views']}>{post.views}회</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default QnAPost;
