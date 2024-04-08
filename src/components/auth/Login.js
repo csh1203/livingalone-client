ebcaf3860b16b26d44029febc23fcc28d2bf4318
seohyunCho <shanny06@naver.com>
2024-04-08 14:37:31 +0900

    Feat: 버튼 클릭 시 계정 찾기 창으로 이동

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -87,7 +87,7 @@ function Login() {
                     </div>
                 </div>
                 <button className={styles['signin-button']} onClick={() => movePage('/join')}>회원가입</button>
-                <div className={styles['forgot-login']}>아이디 | 비밀번호를 잊으셨나요?</div>
+                <div className={styles['forgot-login']} onClick={() => movePage('/findAccount')}>아이디 | 비밀번호를 잊으셨나요?</div>
             </div>
         </div>
     );
