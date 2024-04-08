94cfa731a638ece5125f6e734680f75f0f238dbe
seohyunCho <shanny06@naver.com>
2024-04-08 14:54:51 +0900

    Feat: 결과 메세지 보이기

--- src/components/auth/find-account/ShowPw.js
+++ src/components/auth/find-account/ShowPw.js
@@ -5,13 +5,13 @@ import styles from '../../../css/auth/find-account/ShowPw.module.css'
 
 import SubmitBtn from "./SubmitBtn";
 
-function ShowPw() {
+function ShowPw({ email }) {
     const navigate = useNavigate();
     return(
         <div className={styles['show-pw']}>
             <img src="/images/find-account/pw-icon.svg" className={styles['icon']}/>
             <div className={styles['change-pw']}>
-                holosergi@gmail.com님의 <br/>
+                {email}님의 <br/>
                 비밀번호 변경이 완료되었습니다
             </div>
             <SubmitBtn name="로그인하러 가기" clickEvent={() => navigate('/login')}/>
