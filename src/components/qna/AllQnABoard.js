9ba4142e4ff454cb38f8c22c01628b886f432807
hyeg0121 <w2218@e-mirim.hs.kr>
2024-05-28 22:29:30 +0900

    hotfix: 태깅 로직 수정

--- src/components/qna/AllQnABoard.js
+++ src/components/qna/AllQnABoard.js
@@ -11,25 +11,34 @@ import QnAItem from "./QnAItem";
 
 function AllQnABoard() {
     const [data, setData] = useState([]);
+    const [header, setHeader] = useState('전체 게시글')
     const [tag, setTag] = useState('전체 게시글');
+    const [page, setPage] = useState(1);
 
     // 쿼리 스트링 가져오기
     const tagName = useLocation().search.split('=')[1];
 
-    const setHeaderName = () => {
-        if (tagName === undefined) return '전체 게시글'
-        else if (tagName == 'cost') return '비용'
-        else if (tagName == 'dwelling') return '주거'
-        else if (tagName == 'interior') return '인테리어'
-        else if (tagName == 'tip') return '생활꿀팁'
+    // TODO: 태그 편집하는거 하기
+    const tagging = () => {
+        if (tagName === 'cost') {
+            setTag('비용')
+        } else if (tagName === 'dwelling') {
+            setTag('주거')
+        } else if (tagName === 'interior') {
+            setTag('인테리어')
+        } else if (tagName === 'tip') {
+            setTag('생활꿀팁')
+        }
     }
+
     useEffect(() => {
         fetchData();
-    }, [])
+        tagging();
+    }, [page])
 
     const fetchData = async () => {
         try {
-            const response = await axios.get(`http://localhost:3001/questions/list`);
+            const response = await axios.get(`http://localhost:3001/questions/list/paginated?page=${page}`);
             setData(response.data.questions);
         } catch (error) {
             console.error(error);
@@ -40,15 +49,15 @@ function AllQnABoard() {
         <div className={styles['main']}>
             <div className={styles['title-section']}>
                 <h6>Q&A게시판</h6>
-                <h1>{setHeaderName()}</h1>
+                <h1>{tag}</h1>
             </div>
             <div className={styles['board-section']}>
                 <div className={styles['items-container']}>
                     {
                         data.map(item => (
                             <QnAItem
-                                key={item.id}
-                                id={item.id}
+                                key={item.question_pk}
+                                id={item.question_pk}
                                 title={item.title}
                                 content={item.content}
                                 createdAt={item.createdAt.split('T')[0].split('-').join('.')}
@@ -56,7 +65,7 @@ function AllQnABoard() {
                         ))
                     }
                 </div>
-
+                <button onClick={() => setPage(page => page + 1)}>page up</button>
             </div>
         </div>
     )
