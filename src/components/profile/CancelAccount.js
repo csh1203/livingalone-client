d18983b6249153a0fe654babb31db7aa46074e1e
seohyunCho <shanny06@naver.com>
2024-06-02 17:37:28 +0900

    Feat: 회원 탈퇴 api 연결

--- src/components/profile/CancelAccount.js
+++ src/components/profile/CancelAccount.js
@@ -1,37 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useSelector } from "react-redux";
+import { useNavigate } from 'react-router-dom';
 import '../../css/common/Style.css';
 import styles from '../../css/profile/CancelAccount.module.css'
 
+import RadioBox from './RadioBox';
+import axios from "axios";
+
 function CancelAccount({ setShowCancelPopup }) {
+    const movePage = useNavigate();
+    const userPK = useSelector(state => state.user.userPK);
+
+    const cacelAccount = async() => {
+        try{
+            const response = await axios.delete(`http://127.0.0.1:3001/users/${userPK}`);
+            movePage('/login');
+        }catch(error){
+            console.log(error);
+        }
+        
+    }
+
     return(
         <div className={styles['popup-shadow']}>
             <div className={styles['popup-box']}>
                 <div className={styles['title']}>계정을 탈퇴하시겠습니까?</div>
                 <div className={styles['radio-container']}>
-                    <div className={styles['radio-box']}>
-                        <input type="radio" name="radio" className={styles['radio-input']}/>
-                        <div className={styles['radio-text']}>기록 삭제 목적</div>
-                    </div>
-                    <div className={styles['radio-box']}>
-                        <input type="radio" name="radio" className={styles['radio-input']}/>
-                        <div className={styles['radio-text']}>이용이 불편하고 장애가 많아서</div>
-                    </div>
-                    <div className={styles['radio-box']}>
-                        <input type="radio" name="radio" className={styles['radio-input']}/>
-                        <div className={styles['radio-text']}>다른 사이트가 더 좋아서</div>
-                    </div>  
-                    <div className={styles['radio-box']}>
-                        <input type="radio" name="radio" className={styles['radio-input']}/>
-                        <div className={styles['radio-text']}>사용빈도가 낮아서</div>
-                    </div>
-                    <div className={styles['radio-box']}>
-                        <input type="radio" name="radio" className={styles['radio-input']}/>
-                        <div className={styles['radio-text']}>기타</div>
-                    </div>
-                    <div className={styles['radio-box']}>
-                        <input type="radio" name="radio" className={styles['radio-input']}/>
-                        <div className={styles['radio-text']}>응답 거부</div>
-                    </div>
+                    <RadioBox>기록 삭제 목적</RadioBox>
+                    <RadioBox>이용이 불편하고 장애가 많아서</RadioBox>
+                    <RadioBox>다른 사이트가 더 좋아서</RadioBox>
+                    <RadioBox>사용빈도가 낮아서</RadioBox>
+                    <RadioBox>기타</RadioBox>
+                    <RadioBox>응답 거부</RadioBox>
                 </div>
                 <div className={styles['cancel-notification-box']}>
                     <div className={styles['noti-title']}>
@@ -47,7 +46,7 @@ function CancelAccount({ setShowCancelPopup }) {
 
                 <div className={styles['btns']}>
                     <div className={styles['cancel-btn']} onClick={() => setShowCancelPopup(false)}>아니오</div>
-                    <div className={styles['apply-btn']} onClick={() => setShowCancelPopup(false)}>네</div>
+                    <div className={styles['apply-btn']} onClick={cacelAccount}>네</div>
                 </div>
             </div>
         </div>
