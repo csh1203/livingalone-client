ddf506a8520bc6bb89bdb8afc366a000391ac9d4
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-06 19:27:46 +0900

    Feat: 아티클 검색 로직 추가

--- src/components/article/ArticleSearch.js
+++ src/components/article/ArticleSearch.js
@@ -1,17 +1,37 @@
-import React from "react";
-import styles from "../../css/article/ArticleSearch.module.css"
 import { Icon } from '@iconify/react';
-function ArticleSearch() {
+import React, { useState } from "react";
+import styles from "../../css/article/ArticleSearch.module.css";
+
+function ArticleSearch({ handleOnChange }) {
+    const [value, setValue] = useState("");
+
+    const handleChange = e => {
+        setValue(e.target.value);
+    };
+
+    const handleKeyPress = e => {
+        if (e.key === 'Enter') {
+            handleOnChange(value);
+        }
+    };
+
+    const handleClick = () => {
+        handleOnChange(value);
+    };
+
     return (
         <div id={styles['main']}>
             <div className={styles['search-field-container']}>
                 <input
                     type="text"
                     className={styles['search-field']}
-                    placeholder={"검색어를 입력해주세요. ex)자취방, 금액 등"}
+                    placeholder="검색어를 입력해주세요. ex)자취방, 금액 등"
+                    value={value}
+                    onChange={handleChange}
+                    onKeyPress={handleKeyPress}
                 />
                 <div className={styles['divider']}></div>
-                <Icon icon="ri:search-line" className={styles['search-icon']}/>
+                <Icon icon="ri:search-line" className={styles['search-icon']} onClick={handleClick} />
             </div>
         </div>
     );
