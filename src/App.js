5cdb3ebf10246b2b5fc862f524c05f49670ea826
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-06 22:19:58 +0900

    Feat: ArticleDetail 페이지 추가

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ import {
 import store from './store'; // 추가
 
 import AllQnABoardPage from "./pages/AllQnABoardPage";
+import ArticleDetailPage from "./pages/ArticleDetailPage";
 import ArticlePage from "./pages/ArticlePage";
 import CostCalculatorPage from "./pages/CostCalculatorPage";
 import CreateQnAPage from "./pages/CreateQnAPage";
 import EditInfoPage from "./pages/EditInfoPage";
+import FindAccountPage from "./pages/FindAccountPage";
 import JoinPage from "./pages/JoinPage";
 import LoginPage from "./pages/LoginPage";
 import Main from "./pages/Main";
 import ProfilePage from "./pages/ProfilePage";
 import QnAPage from "./pages/QnAPage";
 import QnAPostPage from "./pages/QnAPostPage";
-import FindAccountPage from "./pages/FindAccountPage";
 
 function App() {
     return (
@@ -30,13 +31,14 @@ function App() {
                     <Route path={"/join"} element={<JoinPage />} />
                     <Route path={"/costCalculator"} element={<CostCalculatorPage />} />
                     <Route path={"/articles"} element={<ArticlePage />} />
+                    <Route path={"/articles/:id"} element={<ArticleDetailPage />} />
                     <Route path={"/qna"} element={<QnAPage />} />
                     <Route path={"/qna/all"} element={<AllQnABoardPage />} />
                     <Route path={"/qna/write"} element={<CreateQnAPage />} />
                     <Route path={"/mypage"} element={<ProfilePage />} />
                     <Route path={"/mypage/editInfo"} element={<EditInfoPage />} />
                     <Route path={"/qna/post/:id"} element={<QnAPostPage />} />
-                    <Route path={"/findAccount"} element={<FindAccountPage/>} />
+                    <Route path={"/findAccount"} element={<FindAccountPage />} />
                 </Routes>
             </Router>
         </Provider>
