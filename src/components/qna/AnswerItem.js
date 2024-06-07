c5a68306f19e2c9764ac53a82d5af22f4d7b6777
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-07 16:00:17 +0900

    Feat: 툴팁 추가

--- src/components/qna/AnswerItem.js
+++ src/components/qna/AnswerItem.js
@@ -1,18 +1,48 @@
+import { useState } from 'react';
 import '../../css/common/Style.css';
 import styles from '../../css/qna/AnswerItem.module.css';
 
-function AnswerItem({ name, content, date }) {
+function AnswerItem({ answerUserPk, name, content, date }) {
+
+    const [isVisible, setIsVisible] = useState(false)
+    const currentUser = localStorage.getItem('userPK');
+    console.log('currentUser', currentUser)
+
+    const handleLineClick = () => {
+        setIsVisible(!isVisible);
+    };
+
+    console.log(process.env.REACT_APP_BASIC_PROFILE)
     return (
         <div className={styles['container']}>
-            <p className={styles['name']}>
-                {name}
-            </p>
+            <div className={styles['profile-dots']}>
+                <div className={styles['profile-name']}>
+                    <img src={process.env.REACT_APP_BASIC_PROFILE} className={styles['profile']} />
+                    <p className={styles['name']}>
+                        {name}
+                    </p>
+                </div>
+                <img src="/images/line.png" className={styles['line']} onClick={() => setIsVisible(isVisible => !isVisible)} />
+                {isVisible && (
+                    <div className={styles['tooltip']}>
+                        {
+                            answerUserPk == currentUser
+                                ?
+                                <p className={styles['tooltip-menu']}>삭제하기</p>
+                                :
+                                <p className={styles['tooltip-error']}>신고하기</p>
+                        }
+                    </div>
+                )}
+            </div>
             <p className={styles['content']}>
                 {content.replace(/<\/?[^>]+(>|$)/g, "")}
             </p>
             <small className={styles['date']}>{date}</small>
+            {/* <p className={styles['accept']}>채택하기</p> */}
         </div>
     )
 }
+
 
 export default AnswerItem;
